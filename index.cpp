adee3d015bee91e0af916b98b91e2d654be41c97
Moataz Mohammed <116386227+m0atazm0hammed@users.noreply.github.com>
2024-05-15 10:00:11 +0300

    fadklfjas

--- index.cpp
+++ index.cpp
@@ -4,7 +4,6 @@
 #include <cstring>
 #include "index.h"
 
-
 indx::indx()
 {
 	primary_keys = {};
@@ -13,55 +12,66 @@ indx::indx()
 
 void indx::write(std::string &fileName, std::string &secondFileName)
 {
-    fstream file(fileName, ios::out | ios::trunc | ios::binary);
-	for (auto& [key, values] : primary_keys) {
-		for (auto& value : values) {
-			file.write((char*)&key, sizeof(key));
+	fstream file(fileName, ios::out | ios::trunc | ios::binary);
+	for (auto &[key, values] : primary_keys)
+	{
+		for (auto &value : values)
+		{
+			file.write((char *)&key, sizeof(key));
 			file.put('|');
-			file.write((char*)&value, sizeof(value));
+			file.write((char *)&value, sizeof(value));
 			file.put('|');
 		}
 	}
 	file.close();
 	file.clear();
 	file.open(secondFileName, ios::out | ios::trunc | ios::binary);
-	for (auto& [key, values] : secondary_keys) {
-		for (auto& value : values) {
-			file.write((char*)&key, sizeof(key));
+	for (auto &[key, values] : secondary_keys)
+	{
+		for (auto &value : values)
+		{
+			file.write((char *)&key, sizeof(key));
 			file.put('|');
-			file.write((char*)&value, sizeof(value));
+			file.write((char *)&value, sizeof(value));
 			file.put('|');
 		}
 	}
 	file.close();
 }
 
-void indx::read(std::string &fileName, std::string &secondFileName) {
+void indx::read(std::string &fileName, std::string &secondFileName)
+{
 	fstream file(fileName, ios::in | ios::binary);
 	if (file.fail())
 		return;
+	file.seekp(0, ios::end);
+	int size = file.tellp();
 	file.seekp(0, ios::beg);
-	while (!file.eof()) {
+	while (file.tellp() < size)
+	{
 		int key;
 		int value;
-		file.read((char*)&key, '|');
-		file.read((char*)&value, '|');
+		file.read((char *)&key, '|');
+		file.read((char *)&value, '|');
 		primary_keys[key].insert(value);
-
 	}
 	file.close();
 	file.clear();
 	file.open(secondFileName, ios::in | ios::binary);
 	if (file.fail())
 		return;
+	file.seekp(0, ios::end);
+	size = file.tellp();
+
 	file.seekp(0, ios::beg);
-	while (!file.eof()) {
+
+	while (file.tellp() < size)
+	{
 		string key;
 		int value;
-		file.read((char*)&key, '|');
-		file.read((char*)&value, '|');
+		file.read((char *)&key, '|');
+		file.read((char *)&value, '|');
 		secondary_keys[key].insert(value);
 	}
 	file.close();
 }
-
