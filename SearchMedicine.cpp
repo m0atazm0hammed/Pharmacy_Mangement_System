ca980837e6e0ca6d083dd1748f78c51a4a1ad164
bluocaroot <assem.akl2004@gmail.com>
2024-05-24 22:34:03 +0300

    final fix

--- SearchMedicine.cpp
+++ SearchMedicine.cpp
@@ -62,8 +62,8 @@ void SearchMedicine::OnSearchById(wxCommandEvent &event)
 		product.LogicalFile.open(product.file_name, ios::in | ios::binary);
 		product.LogicalFile.seekg(offset);
 		product.Read();
-		wxString msg = "ID: " + wxString::Format(wxT("%i"), product.id) + "\nName: " + wxString::FromUTF8(product.name) + "\nPrice: " + wxString::Format(wxT("%i"), product.price) + "\nStock: " + wxString::Format(wxT("%i"), product.stock) + "\nExpiry Date: " + wxString::FromUTF8(product.exp_date);
-		wxMessageBox("Medicine found", "Success", wxOK | wxICON_INFORMATION);
+		wxString msg = "Medicine found ID: " + wxString::Format(wxT("%i"), product.id) + "\nName: " + wxString::FromUTF8(product.name) + "\nPrice: " + wxString::Format(wxT("%i"), product.price) + "\nStock: " + wxString::Format(wxT("%i"), product.stock) + "\nExpiry Date: " + wxString::FromUTF8(product.exp_date);
+		wxMessageBox(msg, "Success", wxOK | wxICON_INFORMATION);
 		product.LogicalFile.close();
 	}
 }
